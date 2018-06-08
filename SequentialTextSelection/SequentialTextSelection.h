07d5f9fe8f9dac59042988721b733d73443d3061
Brychan Odlum <brychan@me.com>
2018-06-08 15:50:36 +0100

    Initial Commit

---
+++ SequentialTextSelection/SequentialTextSelection.h
@@ -0,0 +1,19 @@
+//
+//  SequentialTextSelection.h
+//  SequentialTextSelection
+//
+//  Created by Brychan Bennett-Odlum on 08/06/2018.
+//  Copyright © 2018 Brychan Bennett-Odlum. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SequentialTextSelection.
+FOUNDATION_EXPORT double SequentialTextSelectionVersionNumber;
+
+//! Project version string for SequentialTextSelection.
+FOUNDATION_EXPORT const unsigned char SequentialTextSelectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SequentialTextSelection/PublicHeader.h>
+
+
